da1fa1d1a5a694c621b276da7a3e225743bd52ab
aprtlt26 <117754660+aprtlt26@users.noreply.github.com>
2024-06-08 21:49:34 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -6,19 +6,30 @@ const app = express();
 app.use(bodyParser.json());
 
 const VALID_TOKENS = [
-    'tarot?rc4Q=zuG?0x6o-OjeaZrzFO34gp73EAmgl?0pST0HO=oFAKdNYAbTS?A9KjTtgNSkZDTHHSX5/VnmOc6PcDGiYfnPd0vz?O0ddERwfywE5Pzl1q1yB4eATfDU65f4tjlhttyGPu5j8D9JWYCaaZ/HYjihjc2VV=gSIsgEqjTH5rOKqH7hT5g3c!LaLFckIHPCo?eGG1TcMIRoNKFyPB1NzAjAnmnf5eJV-i6CxmMHVZzU0XuZlB-M3mLjB', 
-    'def456',
-    'ghi789'
+    'tarot?rc4Q=zuG?0x6o-OjeaZrzFO34gp73EAmgl?0pST0HO=oFAKdNYAbTS?A9KjTtgNSkZDTHHSX5/VnmOc6PcDGiYfnPd0vz?O0ddERwfywE5Pzl1q1yB4eATfDU65f4tjlhttyGPu5j8D9JWYCaaZ/HYjihjc2VV=gSIsgEqjTH5rOKqH7hT5g3c!LaLFckIHPCo?eGG1TcMIRoNKFyPB1NzAjAnmnf5eJV-i6CxmMHVZzU0XuZlB-M3mLjB',
+    'otro-token-generado-manualmente',
+    'y-otro-token'
 ];
 
 // Verificar el token y permitir el acceso a la plataforma
-app.post('/verify-token', (req, res) => {
+app.post('/api/verify-token', (req, res) => {
     const { token } = req.body;
 
     if (VALID_TOKENS.includes(token)) {
         res.json({ success: true });
     } else {
         res.status(400).json({ error: 'Token inválido' });
+    }
+});
+
+// Middleware para verificar el token antes de servir la página protegida
+app.get('index.html', (req, res, next) => {
+    const token = req.query.token;
+
+    if (VALID_TOKENS.includes(token)) {
+        next();
+    } else {
+        res.redirect('/verificacion');
     }
 });
 
@@ -36,3 +47,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
